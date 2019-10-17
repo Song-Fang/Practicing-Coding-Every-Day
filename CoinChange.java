9d5e41c371fdd3cf23a504e300936561508854a5
Song(Francis)  Fang <sof10@pitt.edu>
2019-10-17 15:34:53 -0400

    2019/10/17 Coin Change by dynamic programming

---
+++ CoinChange.java
@@ -0,0 +1,32 @@
+package com.algorimthms;
+
+public class CoinChange {
+	public static void main(String [] args) {
+		int [] coins = {1,2,5};
+		CoinChangeSolution ccs = new CoinChangeSolution();
+		System.out.println(ccs.coinChange(4,coins));
+	}
+}
+
+class CoinChangeSolution{
+	public int coinChange(int amount, int [] coins) {
+		if(coins==null||coins.length==0) {
+			return -1;
+		}
+		
+		int [] minAmount = new int[amount+1];
+		minAmount[0] = 0;
+		for(int i =1;i<=amount;i++) {
+			minAmount[i] = Integer.MAX_VALUE;
+			for(int j = 0;j<coins.length;j++) {
+				if(i>=coins[j]&&minAmount[i-coins[j]]!=Integer.MAX_VALUE) {
+					minAmount[i] = Math.min(minAmount[i-coins[j]]+1, minAmount[i]);
+				}
+			}
+		}
+		if(minAmount[amount]==Integer.MAX_VALUE) {
+			return -1;
+		}
+		return minAmount[amount];
+	}
+}
